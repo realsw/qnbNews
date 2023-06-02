9399600d19c76fd93acd608c4cb9eeffa86efc91
realsw <shanwei.zhong@foxmail.com>
2023-06-02 14:34:54 +0800

    update

---
+++ pages/index/index.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class="home">
+
+		<scroll-view scroll-x class="navscroll">
+			<view class="item" :class="index==navIndex ? 'active' : '' " v-for="(item,index) in 10" @click="clickNav(index)">国内</view>
+
+		</scroll-view>
+
+		<view class="content" @click="demo">
+			<view class="row" v-for="item in 10">
+				<newsbox></newsbox>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				navIndex:2
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			//点击导航切换
+			clickNav(index){
+				this.navIndex = index;
+			},
+			demo(){
+				console.log("demo");	
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.navscroll {
+		height: 100rpx;
+		background-color: #F7F8FA;
+		white-space: nowrap;
+		position: fixed;
+		left: 0;
+		top: var(--window-top);
+		z-index: 10;
+	}
+
+	/deep/::-webkit-scrollbar {
+		width: 4px !important;
+		height: 1px !important;
+		overflow: auto !important;
+		background: transparent !important;
+		-webkit-appearance: auto !important;
+		display: block;
+	}
+
+	.item {
+		font-size: 40rpx;
+		display: inline-block;
+		line-height: 100rpx;
+		padding: 0 30rpx;
+		color: #333;
+		&.active{
+			color: #31c27c;
+		}
+	}
+
+	.content {
+		padding:30rpx;
+		padding-top: 130rpx;
+		.row {
+			border-bottom: 1px dotted #efefef;
+			padding: 20rpx 0;
+		}
+	}
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="user">
+		<view class="top">
+			<image src="../../static/images/history.png" mode=""></image>
+			<view class="text">
+				浏览历史
+			</view>
+		</view>
+		<view class="content">
+			<view class="row" v-for="item in 10">
+				<newsbox></newsbox>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.user{
+	.top{
+		padding: 50rpx 0;
+		backgound: #f8f8f8;
+		color: #666;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		image{
+			width: 150rpx;
+			height: 150rpx;
+		}
+		.text{
+			font-size: 38rpx;
+			padding-top: 20rpx;
+		}
+	}
+	.content {
+		padding:30rpx;
+		.row {
+			border-bottom: 1px dotted #efefef;
+			padding: 20rpx 0;
+		}
+	}
+}
+</style>
